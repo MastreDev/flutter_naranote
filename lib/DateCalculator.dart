2bbb9c57d6c40d1a24b149de4bb6612ae639b85a
Mastre <peaceyop@gmail.com>
2024-08-20 10:10:48 +0900

    feat: Target 날짜가 주말인 경우 처리
    Target 날짜가 토요일인 경우 이틀 추가.
    Target 날짜가 일요일인 경우 하루 추가.

--- lib/DateCalculator.dart
+++ lib/DateCalculator.dart
@@ -2,7 +2,16 @@ class DateCalculator {
   int calcDate(DateCalculatorFormat to, DateCalculatorFormat from) {
     DateTime fromDate = DateTime(from.getYear(), from.getMonth(), from.getDay());
     DateTime toDate = DateTime(to.getYear(), to.getMonth(), to.getDay());
-    return toDate.difference(fromDate).inDays;
+
+    if(toDate.weekday == DateTime.saturday) {
+      toDate = toDate.add(const Duration(days: 2));
+    } else if (toDate.weekday == DateTime.sunday) {
+      toDate = toDate.add(const Duration(days: 1));
+    }
+
+    final range = toDate.difference(fromDate).inDays + 1; // 오늘 날짜 포함
+    if (range <= 1) return throw UnsupportedError("Range is too short.");
+    return range < 31 ? 31 : range;
   }
 }
 
--- test/DateCalculator_test.dart
+++ test/DateCalculator_test.dart
@@ -9,45 +9,72 @@ import 'package:naranote/DateCalculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("8월 19일 부터 8월 20일은 1일 차이다.", () {
-    final dateCalc = DateCalculator();
+  final dateCalc = DateCalculator();
 
+  test("8월 19일 부터 8월 20일은 2일 차이다. 31일 이하이기 때문에 31이 나와야 한다.", () {
     const from = "2024-08-19";
     const to = "2024-08-20";
     final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
 
-    expect(dates, 1);
+    expect(dates, 31);
   });
 
   test("8월 18일 부터 8월 20일은 2일 차이다.", () {
-    final dateCalc = DateCalculator();
-
     const from = "2024-08-18";
     const to = "2024-08-20";
     final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
 
-    expect(dates, 2);
+    expect(dates, 31);
   });
 
-  test("같은날짜라면 0을 리턴한다.", () {
-    final dateCalc = DateCalculator();
-
+  test("같은날짜라면 Unsupported 에러가 발생한다.", () {
     const from = "2024-08-19";
     const to = "2024-08-19";
-    final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
 
-    expect(dates, 0);
+    expect(() => dateCalc.calcDate(TestDate.from(to), TestDate.from(from)), throwsA(isA<UnsupportedError>()));
   });
 
-  test("to의 날짜가 오늘보다 이전이라면 -1을 리턴한다. 실제 계산이 되더라도 무조건 -1을 리턴한다.", () {
-    final dateCalc = DateCalculator();
-
+  test("to의 날짜가 오늘보다 이전이라면 Unsupported 에러가 발생한다.", () {
     const from = "2024-08-19";
     const to = "2023-08-19";
 
+    expect(() => dateCalc.calcDate(TestDate.from(to), TestDate.from(from)), throwsA(isA<UnsupportedError>()));
+  });
+
+  test("Given 2024.08.19, 2024.10.02, 기대값은 45일 이다.", () {
+    const from = "2024-08-19";
+    const to = "2024-10-02";
+
     final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
 
-    expect(dates, lessThan(-1));
+    expect(dates, 45);
+  });
+
+  test("Given 2024.08.19, 2024.10.04, 기대값은 47일 이다.", () {
+    const from = "2024-08-19";
+    const to = "2024-10-04";
+
+    final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
+
+    expect(dates, 47);
+  });
+
+  test("Given 2024.08.19, 2024.10.05(토요일), 기대값은 50일 이다. 보정 +2일", () {
+    const from = "2024-08-19";
+    const to = "2024-10-05";
+
+    final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
+
+    expect(dates, 50);
+  });
+
+  test("Given 2024.08.19, 2024.10.06(일요일), 기대값은 50일 이다. 보정 +1일", () {
+    const from = "2024-08-19";
+    const to = "2024-10-06";
+
+    final dates = dateCalc.calcDate(TestDate.from(to), TestDate.from(from));
+
+    expect(dates, 50);
   });
 }
 
